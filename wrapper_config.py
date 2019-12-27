cc87deb2be4bbd7ee2c41aab48a3b9c1fd458276
s1nus0 <54812744+s1nus0@users.noreply.github.com>
2019-12-27 14:26:21 +0300

    Add files via upload

---
+++ wrapper_config.py
@@ -0,0 +1,41 @@
+#coding: utf-8
+
+# proxy types: http|https|socks4|socks5
+
+
+DEBUG = True # False - without logs
+#DEBUG = False # False - without logs
+LOG_FILE = 'wrapper_session.log'
+
+SQLMAP_DUMPS = 'dumps'
+WRAPPER_TXT_DUMPS = 'txt_dumps'
+
+URLS_FILE = 'sites.txt'
+SQLi_SAVE_FILE = 'goodsqli.txt'
+BRUTE_SAVE_FILE = 'goodbrute.txt'
+Check_SQLi = True
+DUMP = False
+PASSW_FILE = 'passw.txt'
+COLUMN_DUMP = 'mail,pass,pwd,phone,ssn,fname,lname'
+Check_List = True
+BRUTE_CMS = False
+PROXY = False # False if work without it
+TAMPER = 'space2plus'
+PROXY_TYPE = 'socks5'
+PROXY_URL = '' # first
+PROXY_FILE = '' # second if not first
+PROXY_USERNAME = ''
+PROXY_PASSWORD = ''
+THREADS = 100
+URLS_LIMIT = 10000000
+DUMP_FOLDER = 'dump'
+DUMP_COLUMN_LIMIT = 20 # 91 - 100 ...
+TIMEOUT = 240 # sec
+RETRIES = 5
+CRAWL = False
+CRAWL_DEP = 1 # глубина
+
+DELETE=True
+
+RISK = 3
+LEVEL = 5
